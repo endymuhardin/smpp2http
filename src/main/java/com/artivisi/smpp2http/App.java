b24d63ab78a8e5bce98c0434bdc649f21902da54
Endy Muhardin <endy.muhardin@gmail.com>
2013-07-21 15:23:56 +0700

    template project

---
+++ src/main/java/com/artivisi/smpp2http/App.java
@@ -0,0 +1,13 @@
+package com.artivisi.smpp2http;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
